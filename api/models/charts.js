8ad44a248eee7eb77a14dea7fe9a527c650e0dc2
Prinya Chaitongrat <basprinya@outlook.com>
2023-03-30 11:44:59 +0700

    ok

---
+++ api/models/charts.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Charts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Charts.init({
+    kwh: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Charts',
+    underscored: true,
+    freezeTableName: true,
+  });
+  return Charts;
+};
