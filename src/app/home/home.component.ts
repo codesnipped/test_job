19fbf3eb8006ec8c5f1c95d200d5345a1ad764b7
Prinya Chaitongrat <basprinya@outlook.com>
2023-03-30 14:16:37 +0700

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ChartService } from '../services/chart.service';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { ChartService } from '../services/chart.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  @ViewChild('chartCanvas') chartCanvas: ElementRef | undefined;
+  dataChart: any;
+  month: any = []
+  kwh: any = []
+  temp: any = []
 
   constructor(private chartService: ChartService){
     this.getChart()
@@ -19,6 +26,39 @@ export class HomeComponent {
         
       }
     })
+  }
+
+  lineChartMethod() {
+    this.dataChart = new Chart(this.chartCanvas?.nativeElement, {
+      type: 'line',
+      data: {
+        labels: this.month,
+        datasets: [
+          {
+            label: 'ยอดขาย',
+            //  lineTension: 0.2, 
+            fill: false,
+            backgroundColor: 'rgba(240, 173, 78,0.4)',
+            borderColor: 'rgba(240, 173, 78,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(240, 173, 78,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(240, 173, 78,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.kwh,
+            spanGaps: false,
+          },
+        ],
+      },
+    });
   }
 
 }
