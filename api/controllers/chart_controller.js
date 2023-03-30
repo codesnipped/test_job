7266187dad34fef88ad40215c870f2171e9f4134
Prinya Chaitongrat <basprinya@outlook.com>
2023-03-30 18:49:36 +0700

    ok

--- api/controllers/chart_controller.js
+++ api/controllers/chart_controller.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
-const { Op, Sequelize } = require("sequelize");
+const { Op, Sequelize, FLOAT } = require("sequelize");
 const date = require('date-and-time');
 
 router.get('/chart', async (req, res) => {
@@ -61,7 +61,7 @@ router.get('/test', async (req, res) => {
 
         const result = await db.Charts.findAll({
             attributes: [
-                [Sequelize.literal("DATE_FORMAT(created_at, '%Y-%m-%d')"), 'month_year'],
+                'createdAt',
                 [Sequelize.fn('MONTH', Sequelize.col('created_at')), 'month'],
                 [Sequelize.fn('YEAR', Sequelize.col('created_at')), 'year'],
                 [Sequelize.fn('AVG', Sequelize.col('kwh')), 'average_kwh'],
@@ -76,10 +76,71 @@ router.get('/test', async (req, res) => {
             } */
         })
 
-        console.log(result);
+        const temp_hight = await db.Charts.findAll({
+            attributes: [
+                [Sequelize.fn('MONTH', Sequelize.col('created_at')), 'month'],
+                [Sequelize.fn('YEAR', Sequelize.col('created_at')), 'year'],
+                [Sequelize.fn('AVG', Sequelize.col('temp')), 'average_temp_hight'],
+            ],
+            group: ['year', 'month'],
+            where: {
+                temp: {
+                    [Op.between]: [70, 1000]
+                }
+                /* createdAt: {
+                    [Op.gte]: new Date('2022-01-01'),
+                    [Op.lt]: new Date('2023-01-01')
+                } */
+            }
+        })
+
+        const temp_low = await db.Charts.findAll({
+            attributes: [
+                [Sequelize.fn('MONTH', Sequelize.col('created_at')), 'month'],
+                [Sequelize.fn('YEAR', Sequelize.col('created_at')), 'year'],
+                [Sequelize.fn('AVG', Sequelize.col('temp')), 'average_temp_low'],
+            ],
+            group: ['year', 'month'],
+            where: {
+                temp: {
+                    [Op.between]: [0, 69]
+                }
+                /* createdAt: {
+                    [Op.gte]: new Date('2022-01-01'),
+                    [Op.lt]: new Date('2023-01-01')
+                } */
+            }
+        })
+
+        /* จัดระเบียบ */
+        month = []
+        avg_kwh = []
+        avg_tem = []
+        avg_temp_hight = []
+        avg_temp_low = []
+        result.forEach((element, index, array) => {
+            month.push(date.format(element.createdAt, 'MMM YYYY'))
+            avg_kwh.push(Number(element.dataValues.average_kwh.toFixed(2)))
+            avg_tem.push(Number(element.dataValues.average_temp.toFixed(2)))
+        });
+        temp_hight.forEach((element, index, array) => {
+            avg_temp_hight.push(Number(element.dataValues.average_temp_hight.toFixed(2)))
+        });
+        temp_low.forEach(async (element, index, array) => {
+            avg_temp_low.push(Number(element.dataValues.average_temp_low.toFixed(2)))
+        });
+        /* จัดระเบียบ end*/
+
 
         if (await result) {
-            res.status(200).json(result)
+            //res.status(200).json({ main: result, temp_hight: temp_hight, temp_low: temp_low })
+            res.status(200).json({ 
+                month: month, 
+                avg_kwh: avg_kwh, 
+                avg_tem: avg_tem, 
+                avg_temp_hight: avg_temp_hight, 
+                avg_temp_low: avg_temp_low 
+            })
         }
     } catch (error) {
         res.status(500).json({ message: error.message })
