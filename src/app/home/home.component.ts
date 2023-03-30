aebad6945a555e0d5a40244ded993e749084c3f5
Prinya Chaitongrat <basprinya@outlook.com>
2023-03-30 14:51:28 +0700

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,29 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ChartService } from '../services/chart.service';
 import Chart from 'chart.js/auto';
 
+const data = {
+  labels: ['1', '2', '3', '4', '5'],
+  datasets: [
+    {
+      label: 'Avg kWh',
+      data: [10, 20, 30, 25, 40],
+      borderColor: '',
+      backgroundColor: '',
+      order: 1,
+      yAxisID: 'y'
+    },
+    {
+      label: 'Avg Low',
+      data: [5, 7, 22, 25, 100],
+      borderColor: '',
+      backgroundColor: '',
+      type: 'line',
+      order: 0,
+      yAxisID: 'y1'
+    }
+  ]
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -23,40 +46,43 @@ export class HomeComponent {
     this.chartService.getChart().subscribe({
       next:(res)=>{
         console.log(res);
-        
+        this.lineChartMethod(data)
       }
     })
   }
 
-  lineChartMethod() {
+  lineChartMethod(datasets: any) {
     this.dataChart = new Chart(this.chartCanvas?.nativeElement, {
-      type: 'line',
-      data: {
-        labels: this.month,
-        datasets: [
-          {
-            label: 'ยอดขาย',
-            //  lineTension: 0.2, 
-            fill: false,
-            backgroundColor: 'rgba(240, 173, 78,0.4)',
-            borderColor: 'rgba(240, 173, 78,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(240, 173, 78,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(240, 173, 78,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: this.kwh,
-            spanGaps: false,
+      type: 'bar',
+      data: datasets,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
           },
-        ],
+          title: {
+            display: true,
+            text: 'ทดสอบสมัครงาน'
+          }
+        },
+        scales: {
+          y: {
+            type: 'linear',
+            display: true,
+            position: 'left',
+          },
+          y1: {
+            type: 'linear',
+            display: true,
+            position: 'right',
+
+            // grid line settings
+            grid: {
+              drawOnChartArea: false, // only want the grid lines for one axis to show up
+            },
+          },
+        }
       },
     });
   }
